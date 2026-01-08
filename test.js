830b2cf7f140a92a6bf1651ac306adbdbc53be72
Goncalo Pinto (CTW) <goncalo.pinto@ctw.bmwgroup.com>
2026-01-08 12:52:24 +0000

    remove redundant instructions from test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
 console.log('Hello, World!');
 
 // This is a simple Node.js script that prints "Hello, World!" to the console.
-// To run this script, save it as "test.js" and execute it using the command: node test.js
