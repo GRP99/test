74f4de2a23ae509bd19cc14ccf0ca0374abbc7fe
Goncalo Pinto (CTW) <goncalo.pinto@ctw.bmwgroup.com>
2026-01-08 12:50:25 +0000

    restrict job execution to main branch for CDC webserver workflow

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.log('Hello, World!');
 
 // This is a simple Node.js script that prints "Hello, World!" to the console.
+// To run this script, save it as "test.js" and execute it using the command: node test.js
