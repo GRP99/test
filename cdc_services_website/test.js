5b97c5a4c60b8b0f022c53c90bd4048eaa497893
Goncalo Pinto (CTW) <goncalo.pinto@ctw.bmwgroup.com>
2026-02-16 16:40:32 +0000

    cancelling

--- cdc_services_website/test.js
+++ cdc_services_website/test.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!!');
+// Function to wait one minute (60 seconds)
+async function waitOneMinute() {
+  return new Promise(resolve => {
+    setTimeout(resolve, 60000); // 60,000 milliseconds = 1 minute
+  });
+}
+
+// Main execution - waits one minute and then proceeds
+async function main() {
+  console.log('Starting wait...');
+  await waitOneMinute();
+  console.log('One minute has passed! Proceeding...');
+}
+
+// Run the main function
+await main();
